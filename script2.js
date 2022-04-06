16c4f3812c5e0e11c6ae08c94e5580c7dcbdc0d2
mansoor <mansoorprojects75@gmail.com>
2022-04-06 20:30:41 +0530

    again

--- script2.js
+++ script2.js
@@ -11,7 +11,7 @@ const links = document.querySelectorAll('.scroll_to')
 links.forEach((item)=>{
 item.addEventListener("click",()=>{
     const el=document.getElementById(item.getAttribute("data-link"));
-    el.scrollIntoView({behavior:"smooth",block:"center"})
+    el.scrollIntoView({behavior:"smooth",block:"end"})
 })
 })
 
